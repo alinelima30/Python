89a655e4ac508544c65c82551924b15ad68369f8
Aline Lima <alimary1994@gmail.com>
2024-09-12 08:18:11 -0300

    Add files via upload
    Nesta aula, tivemos que criar duas variáveis para números, e usar as condicionais "IF" ( SE), para dizer se esses dois números são iguais. E "ELSE" (SE NÃO) para dizer que não são iguais.

---
+++ exercicio02.py
@@ -0,0 +1,11 @@
+a1 = int(input("Digite o 1º número"))
+a2 = int(input("Digite o 2º número"))
+
+if a1 == a2:
+    print("São iguais")
+
+else:
+    if a1 > a2:
+        print(a2,a1)
+    else:
+        print(a1,a2)
